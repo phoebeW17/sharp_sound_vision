be3da0c2a955981cb99bd0db89604a7ba4519e51
Phoebe Whiteley <phoebex17@gmail.com>
2025-01-09 10:08:51 +0000

    initial set up and add media app

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn sharpsound_vision.wsgi
---
+++ sharpsound_vision/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for sharpsound_vision project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sharpsound_vision.settings')
+
+application = get_asgi_application()
