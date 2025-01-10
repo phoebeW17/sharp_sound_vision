b698619d0272aba2ead52c553b9428e56f173be2
Phoebe Whiteley <phoebex17@gmail.com>
2025-01-10 11:14:11 +0000

    add portfolio views and urls

--- sharpsound_vision/urls.py
+++ sharpsound_vision/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from portfolio import views as portfolio_views
 
 urlpatterns = [
+    path('', portfolio_views.home, name='home'),
     path('admin/', admin.site.urls),
 ]
