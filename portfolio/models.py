9cc3b1f9efdff4a5bea602ab18d344942c866e08
Phoebe Whiteley <phoebex17@gmail.com>
2025-01-14 15:56:14 +0000

    add portfolio model and connect cloudinary

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
+STATUS = ((0, "Draft"), (1, "Published"))
 
+class Service(models.Model):
+    title = models.CharField(max_length=100)
+    media = CloudinaryField('media', default='placeholder')
+    description = models.TextField()
+    status = models.IntegerField(choices=STATUS, default=0)
 
 
 
