a05bd4193a3028727fd08c9451bf1d036c5cebb4
Phoebe Whiteley <phoebex17@gmail.com>
2025-01-14 15:39:39 +0000

    add services model

--- services/models.py
+++ services/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Service(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=200)
+    description = models.TextField()
+    example = models.URLField()
+    status = models.IntegerField(choices=STATUS, default=0)
