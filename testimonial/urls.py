467373cd911a092ff541d51f8d825e589fd259bd
Phoebe Whiteley <phoebex17@gmail.com>
2025-01-22 13:30:53 +0000

    fix attribution errors in testimonial views.py

--- testimonial/urls.py
+++ testimonial/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.testimonial_list_view, name='testimonial'),
     path('<int:id>/', views.testimonial_detail_view, name='testimonial-detail'),
-    path('new/', views.new, name='new-testimonial'),
+    path('new/', views.new_testimonial_view, name='new-testimonial'),
     path('edit-testimonial/<int:id>/', views.edit, name='edit-testimonial'),
     path('delete-testimonial/<int:id>/', views.delete, name='delete-testimonial'),
 ]
--- testimonial/views.py
+++ testimonial/views.py
@@ -6,11 +6,6 @@ from django.contrib.auth.decorators import login_required
 # class TestimonialList(generic.ListView):
 #     queryset = Testimonial.objects.filter(status=1).order_by('-posted_on')
 #     template_name = 'testimonial/testimonial.html'
-from django.contrib.auth.decorators import login_required
-
-# class TestimonialList(generic.ListView):
-#     queryset = Testimonial.objects.filter(status=1).order_by('-posted_on')
-#     template_name = 'testimonial/testimonial.html'
 
 def testimonial_list_view(request):
     testimonials = Testimonial.objects.all()
@@ -22,3 +17,23 @@ def testimonial_list_view(request):
 def testimonial_detail_view(request, id):
     testimonial = get_object_or_404(Testimonial, id=id)
     return render(request, 'testimonial/testimonial-details.html', {'testimonial': testimonial})
+
+def new_testimonial_view(request):
+    if request.method == 'POST':
+        # Handle form submission here
+        pass
+    return render(request, 'testimonial/new-testimonial.html')
+
+def edit(request, id):
+    testimonial = get_object_or_404(Testimonial, id=id)
+    if request.method == 'POST':
+        # Handle form submission here
+        pass
+    return render(request, 'testimonial/edit-testimonial.html', {'testimonial': testimonial})
+
+def delete(request, id):
+    testimonial = get_object_or_404(Testimonial, id=id)
+    if request.method == 'POST':
+        testimonial.delete()
+        return redirect('testimonial')
+    return render(request, 'testimonial/delete-testimonial.html', {'testimonial': testimonial})
